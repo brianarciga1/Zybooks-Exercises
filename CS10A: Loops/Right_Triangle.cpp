fe3de168854a7cbf4339b41198eb56da9b05b807
brianarciga1 <72134396+brianarciga1@users.noreply.github.com>
2022-06-20 21:43:52 -0700

    Create Right_Triangle.cpp

---
+++ CS10A: Loops/Right_Triangle.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   char triangleChar;
+   int triangleHeight;
+
+   cout << "Enter a character:" << endl;
+   cin >> triangleChar;
+   
+   cout << "Enter triangle height:" << endl;
+   cin >> triangleHeight;
+   cout << endl;
+   for(int i=0; i< triangleHeight; i++){
+      for(int j=0; i >= j ; j++){
+         cout << triangleChar << " ";
+      }
+      cout << endl;
+   }
+   return 0;
+}
