8a5f5e38c3fcb19182d809b26650777f64ba069c
brianarciga1 <72134396+brianarciga1@users.noreply.github.com>
2023-06-13 00:53:51 -0700

    Update WritingVectorsWithClasses(GoodExample).cpp

--- CS10B: Objects-Classes/WritingVectorsWithClasses(GoodExample).cpp
+++ CS10B: Objects-Classes/WritingVectorsWithClasses(GoodExample).cpp
@@ -5,43 +5,43 @@ using namespace std;
 
 class Song {
    public:
-      void SetDurationAndName(int songDuration, string songName) {
-         duration = songDuration;
-         name = songName;
+      void SetDurationAndName(int songDuration, string songName) { //function inside of Song
+         duration = songDuration; //set to Song's private member(duration)
+         name = songName; //set to Song's private member(name)
       }
-      void PrintSong() const {
+      void PrintSong() const { //simple print function
          cout << duration << " - " << name << endl;
       }
-      int GetDuration() const { return duration; }
-      string GetName() const { return name; }
+      int GetDuration() const { return duration; } //only returns duration
+      string GetName() const { return name; } //only returns name
 
    private:
       int duration;
-      string name;
+      string name; //Song's private name
 };
 
 class Album {
    public:
-      void SetName(string albumName) { name = albumName; }
-      void InputSongs();
-      void PrintName() const { cout << name << endl; }
-      void PrintSongsShorterThan(int songDuration) const;
+      void SetName(string albumName) { name = albumName; } //function sets Album's private name
+      void InputSongs(); 
+      void PrintName() const { cout << name << endl; } //function prints name after updating
+      void PrintSongsShorterThan(int songDuration) const; // it'll print songs short than, in this case, 120 (see main)
 
    private:
       string name;
-      vector<Song> albumSongs;
+      vector<Song> albumSongs; //private vector of type "Song", meaning it can use duration and name from "Song" (private variables)
 };
 
 void Album::InputSongs() {
-   Song currSong;
+   Song currSong; // calling "Song" class so you can use its stuff
    int currDuration;
    string currName;
 
    cin >> currDuration;
    while (currDuration >=  0) {
       getline(cin, currName);
-      currSong.SetDurationAndName(currDuration, currName);
-      albumSongs.push_back(currSong);
+      currSong.SetDurationAndName(currDuration, currName); // sets current duration and name for "Song"
+      albumSongs.push_back(currSong); // can use "Album" private variables because it is in the same class BUT the variables are from "Song"
       cin >> currDuration;
    }
 }
@@ -50,10 +50,10 @@ void Album::PrintSongsShorterThan(int songDuration) const {
    unsigned int i;
    Song currSong;
 
-   cout << "Songs shorter than " << songDuration << " seconds:" << endl;
+   cout << "Songs shorter than " << songDuration << " seconds:" << endl; 
 
   for(i=0; i < albumSongs.size(); i++) {
-      currSong = albumSongs.at(i);
+      currSong = albumSongs.at(i); // here you can set it to currSong because we want to use "Song's" private variables
       if(currSong.GetDuration() < songDuration) {
         currSong.PrintSong();
       }
